4f39023201b7980c2042d2882f9ddad8a2e7df49
unknown <namlong9012@gmail.com>
2020-08-20 01:37:27 +0700

    lat anh + xet dung sai

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Square from './components/Square'
 
 function App() {
-  const list = [];
-  for(let i=1; i<=64; i++){
-    let color = '';
-    const row = Math.ceil(i/8); 
-    const check = row + i;
-    if(check%2 !== 0)
-      color = 'pink';
-    else 
-      color = 'white';
-    
-    const col = (i-1)%8;
-    let char = String.fromCharCode(col+65);
-    list.push(<Square colorSquare={color} index={row} char={char}></Square>)
+  const [result, setResult] = useState();
+
+  const listId = [];
+  const pushId = (id) => {
+    listId.push(id)
+    console.log(listId);
+  }  
+
+  const checkId = (id) => {
+    if(listId.length !== 1){
+      if(id === listId[0]){
+        listId.splice(0, listId.length);
+        console.log("chon dung");
+      }
+      else{
+        console.log("chon sai");
+      }
+    }  
   }
+
+  const shuffle = (array) => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+  let list = [];
+  for(let i=1; i<=16; i++){
+    const id = i%8;
+    list.push(<Square pushId={pushId} id={id} checkId={checkId}></Square>);
+  }
+
   return (
-    <div className="container">
-      {list}
+    <div className='container'>
+        {shuffle(list)}
     </div>
   )
 }
--- my-app/src/components/Square.js
+++ my-app/src/components/Square.js
@@ -1,19 +1,27 @@
 import React, { useState } from "react"
 import '../App.css';
 
-function Square({colorSquare, index, char}){
-    const [color, setColor] = useState(colorSquare);
+function Square({ pushId, checkId, id, result }){   
+    const urlImage = "https://loremflickr.com/300/300?lock=" + id; 
+    const [opacity, setOpacity] = useState(0);
 
-    const changeColor = () => {
-        if(color === 'pink')
-            setColor('orange');
-    }  
+    const flip = () => {  
+        setOpacity(1);
+        pushId(id);
+        checkId(id);
+        // if(!result){
+        //     setTimeout(setOpacity(0), 2000);
+        // } else{
+        //     setOpacity(1);
+        // }
+    }
     
     return (
-        <div className="square" onClick={() => changeColor()} style={{ backgroundColor : color}}>     
-            {index + char}
+        <div className="square" onClick={() => flip()}>
+            <img src={urlImage} style={{ opacity : opacity}} ></img>     
         </div>
     );
+   
 }
 
 export default Square;
