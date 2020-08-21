39567951057b84ff0ba307890b042ddf77916e56
namanh.nguyen <namanh6669@gmail.com>
2020-08-21 11:39:17 +0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,46 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Square from './components/Square'
 
 function App() {
-  // const [status, setStatus] = useState();
   const [wrongId, setWrongId] = useState([]);
+  const [isChecking, setIsChecking] = useState(false);
 
   const listId = [];
   const listTrueId = [];
-
-  const checkId = (id) => {   
-    if(listTrueId.indexOf(id) === -1) 
-      listId.push(id);
-
-    if(listId.length === 2){
-      if(listId[0] === listId[1]){
+  const listWrongId = [];
+  // tao list anh
+  const createImages = (list = []) => {
+    if (list.length) return list;
+    for (let i = 1; i <= 16; i++) {
+      const id = i % 8;
+      list.push(<Square id={id} checkId={checkId} wrongId={wrongId}></Square>);
+    }
+    return list
+  };
+  // check
+  const checkId = (id) => {
+    if (listTrueId.indexOf(id) === -1) listId.push(id);
+    if (listId.length === 2) {
+      // disable click
+      setIsChecking(true);
+      if (listId[0] === listId[1]) {
         listTrueId.push(listId[0]);
         console.log('chon dung');
+        // enable click
+        setIsChecking(false);
       }
-      else if(listId[0] !== listId[1]){
+      else if (listId[0] !== listId[1]) {
         console.log('chon sai', listId);
-        setWrongId(listId);
+        listWrongId.push(...listId);
+        setTimeout(() => {
+          console.log('chon tiep');
+          setWrongId(listWrongId);
+          // enable click
+          setIsChecking(false);
+        }, 2000)
       }
       listId.splice(0, 2);
     }
-
-    if(listTrueId.length === 8)
-      alert('Win roi');
+    if (listTrueId.length === 8) alert('Win roi');
   }
-
-
-
-  let list = [];
-  for(let i=1; i<=16; i++){
-    const id = i%8;
-    list.push(<Square id={id} checkId={checkId} wrongId={wrongId}></Square>);
-  }
-
   return (
-    <div className='container'>
-        {list}
+    <div style={{ pointerEvents: isChecking ? 'none' : 'auto' }} className='container'>
+      {createImages()}
     </div>
   )
 }
--- my-app/src/components/Square.js
+++ my-app/src/components/Square.js
@@ -1,25 +1,25 @@
 import React, { useState, useEffect } from "react"
 import '../App.css';
 
-function Square({ checkId, id, wrongId }){   
-    const urlImage = "https://loremflickr.com/300/300?lock=21" + id; 
+function Square({ checkId, id, wrongId }) {
+    const urlImage = "https://loremflickr.com/300/300?lock=21" + id;
     const [visibility, setVisibility] = useState('hidden');
 
-    const flip = () => {  
-        setVisibility(''); 
-        checkId(id);  
+    const flip = () => {
+        setVisibility('');
+        checkId(id);
     }
 
-    useEffect((id) => {
-        console.log(wrongId, id);
+    useEffect(() => {
+        const isVisible = (wrongId.indexOf(id) !== -1) ? 'hidden' : null;
+        if (isVisible) setVisibility(isVisible)
     }, [wrongId])
-    
     return (
         <div className="square" onClick={() => flip()}>
-            <img src={urlImage} style={{ visibility : ((wrongId.indexOf(id) !== -1) ? 'hidden' : visibility) }} alt=""></img>     
+            <img src={urlImage} style={{ visibility: visibility }} alt=""></img>
         </div>
     );
-   
+
 }
 
 export default Square;
